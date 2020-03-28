31c5f80b1a6bb99b27fa3dacfba2588a6dccfb7f
Andi Hoerduin <andihoerudin24@gmail.com>
2020-03-28 14:31:53 +0700

    146 done

---
+++ store/reducers/meals.js
@@ -0,0 +1,16 @@
+import {MEALS} from '../../data/dummy-data'
+
+const intialState ={
+    meals:MEALS,
+    filterMelas:'',
+    favoriteMeals:[]
+}
+
+
+const mealsReducer =(state=intialState,action) => {
+    
+    return state;
+}
+
+
+export default mealsReducer
